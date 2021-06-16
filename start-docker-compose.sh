3c6abe5c2bc57a87f16c5cf4895d10cc4a219943
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-06-16 15:05:07 +0700

    move to folder maintain

--- start-docker-compose.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Starting docker compose"
-docker-compose -p myriad-db up -d
--- stop-docker-compose.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Stopping docker compose"
-docker-compose -p myriad-db down -v
